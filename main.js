d508abf9854bf5f77f3712417b08385582d54444
Ronny <ronnykuruvilla@gmail.com>
2021-03-09 13:58:48 -0500

    complete.

--- main.js
+++ main.js
@@ -7,53 +7,70 @@ const myCodeImmersivesObj = {
 // You won't change lines 1-3 above, but instead set and change the properties for each step below by writing NEW code underneath each comment.
 
 // Below, set the value of property `wait` to be "I object".
-
+myCodeImmersivesObj.wait = 'I object'
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
-
+myCodeImmersivesObj.isAnExcellentObject = true;
 
 // Add the property `term` and set it to 1.
-
+myCodeImmersivesObj.term = 1;
 
 // Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
-
+myCodeImmersivesObj.term = myCodeImmersivesObj.term = +1
 
 // Now add a new property called `currentScore` and set it to 3.
-
+myCodeImmersivesObj.currentScore = 3;
 
 // Now add a new property called `cumulativeScore` and set it to 50.
-
+myCodeImmersivesObj.cumulativeScore = 50;
 
 // Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
-
+myCodeImmersivesObj.currentScore = myCodeImmersivesObj.cumulativeScore = myCodeImmersivesObj.cumulativeScore
 
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
-
+myCodeImmersivesObj ["i am exactly this cool"] = 'sooooo cool'
 
 // Part 2 - your own object.
 // Now make a new EMPTY object that represents yourself. You won't put any properties in the curly braces, but just like in part 1, add them in each step below using plain JavaScript statements.
-
+const allAboutMe = {};
 
 // Add a `firstName` property and set it to a string.
-
-
+allAboutMe.firstName = 'Ronny';
 // Add a `lastName` property and set it to a string.
-
+allAboutMe.lastName = 'P';
 
 // Add a `fullName` property and set it to the name properties put together with a space string in the middle.
+allAboutMe.fullName = allAboutMe.firstName + ' ' + allAboutMe.lastName
 
 
 // Add an `age` property and set it to a number.
-
+allAboutMe.age = 23;
 
 // Add an address property and set it to an empty array.
-
+allAboutMe.address = []
 
 // Now push three strings into that array.
-
+allAboutMe.address.street = 'FourFive ave.'
+allAboutMe.address.town = 'SevenEight'
+allAboutMe.address.state = 'Nine Ten'
+console.log(allAboutMe)
 
 // Part 3 - setting key-value pairs in-line.
-// Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `codeImmersivesObject` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of numbers. The important thing here is: this should all be done within the brackets of the object, not below the place where the object is declared.
+// Now make a new object, but this time we'll pre-populate it, 
+//not set it in any lines below it. In other words, 
+//the properties should be set the same way I initially set the `codeImmersivesObject` 
+//on line 1. It can be called whatever you want and can have whatever properties you 
+//want, as long as they include at least one string value, boolean value, number value,
+// and array of numbers. The important thing here is: this should all be done within the brackets of the object, 
+//not below the place where the object is declared.
+const doIKnowYou = {
+  iThinkSo: 'lol just kidding',
+  butDoI: false,
+  howManyJellyBeans: 199,
+  myArray: [1,2,3,4,5]
+}; 
+console.log(doIKnowYou)
 
-
-// As a stretch goal, go back pre-populate the object with at least one property using a key name that CAN'T be a variable name. That would be anything with a space in it, or a dash, or starting with a number.
+// As a stretch goal, go back pre-populate the object with at least one property 
+//using a key name that CAN'T be a variable name. That would be anything with a 
+//space in it, or a dash, or starting with a number.
